1ae2d4d23bed30d2f918c69e1caf7019b823ffe5
lunacat <706827455@qq.com>
2017-09-08 18:08:00 +0800

    add button module v0.1

--- components/button/button.vue
+++ components/button/button.vue
@@ -1,23 +1,128 @@
 <template>
-  <button class="vm-button">Button22</button>
+  <a class="vm-button" :disabled="disabled"
+    @mousedown="click()"
+    :role="buttonType"
+    :class="[
+      type ? 'vm-button-' + type : '',
+      size ? 'vm-button-' + size : '',
+      {
+        'vm-button-disabled' : disabled,
+        'vm-button-loading' : loading,
+        'vm-button-inline' : inline,
+        'vm-button-active' : active
+      }
+    ]"
+  >
+    <!-- <i></i> -->
+    <slot></slot>
+  </a>
 </template>
 
 <script>
 export default {
-  name: 'VButton'
+  name: 'VButton',
+  data() {
+    return {
+      active: false
+    }
+  },
+  props: {
+    buttonType: {
+      type: String,
+      default: 'button'
+    },
+    type: String,
+    size: String,
+    disabled: Boolean,
+    loading: Boolean,
+    inline: Boolean
+  },
+  methods: {
+    click () {
+      console.log(3)
+    }
+  }
 }
 </script>
 
 <style lang="less">
-  @buttonprefixCls: 'vm-button';
-  .@{buttonprefixCls} {
-    width: 100px;
-    line-height: 36px;
-  }
-  .vm-button{
-    width: 100px;
-    line-height: 36px;
-    background: #ccc;
+  @import '../style/mixins';
+  @import '../style/themes/default';
+
+  @buttonPrefixCls: vm-button;
+  .@{buttonPrefixCls} {
+    display: block;
+    outline: 0 none;
+    -webkit-appearance: none;
+    box-sizing: border-box;
+    padding: 0;
+    text-align: center;
+    font-size: @button-font-size;
+    height: @button-height;
+    line-height: @button-height;
+    border-radius: @radius-md;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-word;
+    white-space: nowrap;
+
+    // default
+    color: @color-text-base;
+    background-color: @fill-base;
+    border: @border-width-sm solid @border-color-base;
+
+    &&-active {
+      background-color: @fill-tap;
+    }
+
+    &-primary {
+      color: @color-text-base-inverse;
+      background-color: @primary-button-fill;
+      border: @border-width-sm solid @primary-button-fill;
+
+      &.@{buttonPrefixCls}-active {
+        background-color: @primary-button-fill-tap;
+      }
+    }
+
+    &-inline {
+      display: inline-block;
+      padding: 0 @h-spacing-lg;
+    }
+
+    &-small {
+      font-size: @button-font-size-sm;
+      height: @button-height-sm;
+      line-height: @button-height-sm;
+      padding: 0 @h-spacing-lg;
+      border-radius: @radius-md;
+    }
+
+    &-ghost {
+      color: @ghost-button-color;
+      background-color: transparent;
+      border: @border-width-sm solid @ghost-button-color;
+
+      &.@{buttonPrefixCls}-active {
+        color: @color-text-base-inverse;
+        background-color: @ghost-button-fill-tap;
+        border: @border-width-sm solid @ghost-button-color;
+      }
+    }
+
+    &-warning {
+      color: #f86e21;
+      background-color: @fill-base;
+    }
+
+    & {
+      &.@{buttonPrefixCls}-disabled {
+        color: @color-text-disabled;
+        background-color: @fill-disabled;
+        border: 0;
+      }
+    }
   }
 </style>
 
--- components/button/demo/basic.vue
+++ components/button/demo/basic.vue
@@ -1,12 +1,47 @@
 <template>
-  <v-button />
+  <div class="demo-wingblank">
+    <div class="btn-container">
+      <v-button class="btn" type="primary">primary button</v-button>
+      <v-button class="btn" disabled>disabled button</v-button>
+      <v-button class="btn" loading>(icon) loading button</v-button>
+
+      <div style="height: .16rem;" />
+
+      <p>inline / small</p>
+      <div class="demo-flexbox">
+        <v-button class="btn" type="primary" inline>inline</v-button>
+        <v-button class="btn" type="ghost" inline>inline</v-button>
+        <v-button class="btn" type="warning" inline>inline</v-button>
+      </div>
+      <div class="demo-flexbox">
+        <v-button class="btn" type="primary" inline size="small">inline small</v-button>
+        <v-button class="btn" type="ghost" inline size="small">inline small</v-button>
+        <v-button class="btn" type="warning" inline size="small">inline small</v-button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import VButton from '../'
-export default {
-  components: {
-    VButton
+  import VButton from '../'
+  export default {
+    components: {
+      VButton
+    }
   }
-}
 </script>
+
+<style lang="less">
+  .demo-wingblank {
+    margin: 0 .3rem;
+  }
+
+  .demo-flexbox {
+    display: flex;
+    align-items: center;
+  }
+
+  .btn-container .btn {
+    margin: .16rem .13rem 0;
+  }
+</style>
 
